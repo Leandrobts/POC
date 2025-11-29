c119da0dbea9edae8d0105060fa68e50eadfa041
Leandrobts <kazaalite_601@hotmail.com>
2025-11-29 19:46:20 -0300

    Update and rename config.mjs to config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+export const CONFIG = {
+    KERNEL_BASE: null, 
+    WORKER_LIMIT: 403,
+    SIZES_TO_TEST: [0x20, 0x40, 0x80, 0x100, 0x180, 0x200, 0x400, 0x800, 0x1000, 0x2000],
+    SPRAY_QUANTITY: 5000
+};
