8a4a2b8b5333a0c04f00979d630d389106891860
Leandrobts <kazaalite_601@hotmail.com>
2025-11-29 22:30:04 -0300

    Update heap.mjs

--- heap.mjs
+++ heap.mjs
@@ -2,21 +2,27 @@ import { log } from './utils.mjs';
 
 var grooming_stash = [];
 
-export function prepare_heap() {
-    log("HEAP: Preparando Checkerboard (0x400)...");
+// EXPORTAÇÃO EXPLÍCITA
+export function prepare_checkerboard_heap(size) {
+    log(`HEAP: Preparando 0x${size.toString(16)}...`);
     grooming_stash = [];
     
     let temp = [];
-    // Aloca 2000 buffers de 1024 bytes
+    // Aloca 2000 buffers
     for(let i=0; i<2000; i++) {
-        temp.push(new ArrayBuffer(0x400));
+        temp.push(new ArrayBuffer(size));
     }
     
-    // Cria buracos (Libera 1 a cada 2)
+    // Libera alternado para criar buracos
     for(let i=0; i<temp.length; i+=2) {
         temp[i] = null;
     }
     
     grooming_stash = temp.filter(x => x !== null);
     log("HEAP: Pronto.", "success");
+}
+
+// Função auxiliar necessária para limpeza
+export function clear_heap() {
+    grooming_stash = [];
 }
