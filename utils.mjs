bcfca0cef04783ffa5386b94059a5c414383edb0
Leandrobts <kazaalite_601@hotmail.com>
2025-11-30 17:58:58 -0300

    Update utils.mjs

--- utils.mjs
+++ utils.mjs
@@ -1,23 +1,17 @@
 export function log(msg, type = "info") {
     const d = document.getElementById("log");
     if(!d) return;
-    
     let color = "#ccc";
     if(type === "success") color = "#2ea043";
     if(type === "fail") color = "#ff7b72";
     if(type === "warn") color = "#d29922";
-
-    const div = document.createElement("div");
-    div.style.color = color;
-    div.innerHTML = `[${new Date().toLocaleTimeString()}] ${msg}`;
-    d.appendChild(div);
+    d.innerHTML += `<div style="color:${color}">[${new Date().toLocaleTimeString()}] ${msg}</div>`;
     d.scrollTop = d.scrollHeight;
 }
 
 export function hexToBytes(hex) {
-    const len = hex.length / 2;
-    const bytes = new Uint8Array(len);
-    for (let i = 0; i < len; i++) {
+    const bytes = new Uint8Array(hex.length / 2);
+    for (let i = 0; i < bytes.length; i++) {
         bytes[i] = parseInt(hex.substr(i * 2, 2), 16);
     }
     return bytes;
