c97b0cb5b1bce90f9ad1de43c77885c9e7a65c10
Leandrobts <kazaalite_601@hotmail.com>
2026-04-24 09:29:47 -0300

    Update mod_factory.js

--- mod_factory.js
+++ mod_factory.js
@@ -1,24 +1,24 @@
 /**
  * MOD_FACTORY.JS — Gerenciador de Cenários Modular
- * Importa os testes individuais da pasta e ignora o AudioContext.
+ * Importa os testes individuais da pasta /scenarios e ignora o AudioContext.
  */
 
-import scArrayOverflow from './sc_array_int_overflow.js';
-import scCssAnim from './sc_css_anim_removed.js';
-import scCssCustom from './sc_css_custom_prop.js';
-import scDomEvent from './sc_dom_event_removed.js';
-import scIframeUaf from './sc_iframe_frame_uaf.js';
-import scMediaSource from './sc_mediasource_uaf.js';
-import scMessagePort from './sc_messageport_uaf.js';
-import scNativeCallback from './sc_native_callback_uaf.js';
-import scPromiseMicro from './sc_promise_microtask.js';
-import scRegexpOverflow from './sc_regexp_overflow.js';
-import scStringOverflow from './sc_string_int_overflow.js';
-import scStructuredClone from './sc_structured_clone.js';
-import scSvgFilter from './sc_svg_filter_uaf.js';
-import scTreewalker from './sc_treewalker_confusion.js';
-import scVideoFullscreen from './sc_video_fullscreen_remove.js';
-import scWeakmapEphemeron from './sc_weakmap_ephemeron.js';
+import scArrayOverflow from './scenarios/sc_array_int_overflow.js';
+import scCssAnim from './scenarios/sc_css_anim_removed.js';
+import scCssCustom from './scenarios/sc_css_custom_prop.js';
+import scDomEvent from './scenarios/sc_dom_event_removed.js';
+import scIframeUaf from './scenarios/sc_iframe_frame_uaf.js';
+import scMediaSource from './scenarios/sc_mediasource_uaf.js';
+import scMessagePort from './scenarios/sc_messageport_uaf.js';
+import scNativeCallback from './scenarios/sc_native_callback_uaf.js';
+import scPromiseMicro from './scenarios/sc_promise_microtask.js';
+import scRegexpOverflow from './scenarios/sc_regexp_overflow.js';
+import scStringOverflow from './scenarios/sc_string_int_overflow.js';
+import scStructuredClone from './scenarios/sc_structured_clone.js';
+import scSvgFilter from './scenarios/sc_svg_filter_uaf.js';
+import scTreewalker from './scenarios/sc_treewalker_confusion.js';
+import scVideoFullscreen from './scenarios/sc_video_fullscreen_remove.js';
+import scWeakmapEphemeron from './scenarios/sc_weakmap_ephemeron.js';
 
 export const Factory = {
     buildScenarios: function() {
