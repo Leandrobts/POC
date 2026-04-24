01da54bde086bba7bb06f7b937dfbfd922709278
Leandrobts <kazaalite_601@hotmail.com>
2026-04-24 11:36:16 -0300

    Update mod_factory.js

--- mod_factory.js
+++ mod_factory.js
@@ -3,7 +3,9 @@
  * Importa os testes individuais da pasta /scenarios e ignora o AudioContext.
  */
 
-import scArrayOverflow from './scenarios/sc_array_int_overflow.js';
+import scRegexpOverflow from './scenarios/sc_regexp_overflow.js';
+
+/*import scArrayOverflow from './scenarios/sc_array_int_overflow.js';
 import scCssAnim from './scenarios/sc_css_anim_removed.js';
 import scCssCustom from './scenarios/sc_css_custom_prop.js';
 import scDomEvent from './scenarios/sc_dom_event_removed.js';
@@ -12,14 +14,13 @@ import scMediaSource from './scenarios/sc_mediasource_uaf.js';
 import scMessagePort from './scenarios/sc_messageport_uaf.js';
 import scNativeCallback from './scenarios/sc_native_callback_uaf.js';
 import scPromiseMicro from './scenarios/sc_promise_microtask.js';
-import scRegexpOverflow from './scenarios/sc_regexp_overflow.js';
 import scStringOverflow from './scenarios/sc_string_int_overflow.js';
 import scStructuredClone from './scenarios/sc_structured_clone.js';
 import scSvgFilter from './scenarios/sc_svg_filter_uaf.js';
 import scTreewalker from './scenarios/sc_treewalker_confusion.js';
 import scVideoFullscreen from './scenarios/sc_video_fullscreen_remove.js';
 import scWeakmapEphemeron from './scenarios/sc_weakmap_ephemeron.js';
-
+*/
 export const Factory = {
     buildScenarios: function() {
         const allScenarios = [
