46bfb02a0103435966dcdfa84e85d207e0378716
Leandrobts <kazaalite_601@hotmail.com>
2026-04-25 13:08:16 -0300

    Update mod_factory.js

--- mod_factory.js
+++ mod_factory.js
@@ -12,7 +12,7 @@ import scArrayOverflow from './scenarios/sc_array_int_overflow.js';
 import scCssAnim from './scenarios/sc_css_anim_removed.js';
 import scCssCustom from './scenarios/sc_css_custom_prop.js';
 import scDomEvent from './scenarios/sc_dom_event_removed.js';
-import scMediaSource from './scenarios/sc_mediasource_uaf.js';
+//import scMediaSource from './scenarios/sc_mediasource_uaf.js';
 import scMessagePort from './scenarios/sc_messageport_uaf.js';
 import scNativeCallback from './scenarios/sc_native_callback_uaf.js';
 import scPromiseMicro from './scenarios/sc_promise_microtask.js';
@@ -43,7 +43,7 @@ export const Factory = {
             scCssCustom,
             scDomEvent, 
             scDomRange,
-            scMediaSource,                   
+          //  scMediaSource,                   
             scCanvasImage,
             scMessagePort,
             scNativeCallback,
