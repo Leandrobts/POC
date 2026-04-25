5276772ac1eb911fe932858d5d3286b2dbd02305
Leandrobts <kazaalite_601@hotmail.com>
2026-04-24 21:41:57 -0300

    Update mod_factory.js

--- mod_factory.js
+++ mod_factory.js
@@ -3,13 +3,13 @@
  * MOD_FACTORY.JS — Gerenciador de Cenários Modular
  * MODO PRODUÇÃO: Todos os 16 cenários ativos para varredura contínua.
  */
+import scIframeUaf from './scenarios/sc_iframe_frame_uaf.js';
 
-
+/*
 import scArrayOverflow from './scenarios/sc_array_int_overflow.js';
 import scCssAnim from './scenarios/sc_css_anim_removed.js';
 import scCssCustom from './scenarios/sc_css_custom_prop.js';
 import scDomEvent from './scenarios/sc_dom_event_removed.js';
-import scIframeUaf from './scenarios/sc_iframe_frame_uaf.js';
 import scMediaSource from './scenarios/sc_mediasource_uaf.js';
 import scMessagePort from './scenarios/sc_messageport_uaf.js';
 import scNativeCallback from './scenarios/sc_native_callback_uaf.js';
@@ -22,7 +22,7 @@ import scSvgFilter from './scenarios/sc_svg_filter_uaf.js';
 import scWeakmapEphemeron from './scenarios/sc_weakmap_ephemeron.js';
 import scButterfly from './scenarios/sc_butterfly_splice.js';
 import scFinalization from './scenarios/sc_finalization_race.js';
-/*import scFullscreen from './scenarios/sc_fullscreen_api_race.js';
+import scFullscreen from './scenarios/sc_fullscreen_api_race.js';
 import scVideoNative from './scenarios/sc_video_native_fs_swap.js';
 import scVideoFullscreen from './scenarios/sc_video_fullscreen_remove.js';
 */
@@ -30,17 +30,17 @@ import scVideoFullscreen from './scenarios/sc_video_fullscreen_remove.js';
 
 export const Factory = {
     buildScenarios: function() {
-        const allScenarios = [  
-         //   scFullscreen,
-          //  scVideoNative,
+        const allScenarios = [  scIframeUaf
+         /* scFullscreen,
+            scVideoNative,
             scFinalization,
             scButterfly,              
             scArrayOverflow,
             scCssAnim,
             scCssCustom,
-            scDomEvent,
-            scIframeUaf,
-            scMediaSource,
+            scDomEvent, 
+            
+         /  scMediaSource,
             scMessagePort,
             scNativeCallback,
             scPromiseMicro,
@@ -49,8 +49,8 @@ export const Factory = {
             scStructuredClone,
             scTreewalker,
             scSvgFilter,          
-         //   scVideoFullscreen,
-            scWeakmapEphemeron
+            scVideoFullscreen,
+            scWeakmapEphemeron*/
         ];
 
         const activeList = [];
