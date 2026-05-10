286a779a1a9f9ff7cca0fcf954c69737b9729063
Leandrobts <kazaalite_601@hotmail.com>
2026-05-10 08:25:21 -0300

    Update mod_factory.js

--- mod_factory.js
+++ mod_factory.js
@@ -65,7 +65,7 @@ export const Factory = {
             //  scShadowSlot,      
           //  scAudioNode,     
          //   scRangeBoundary, 
-          //  scCanvasContext,
+          scCanvasContext,
             scCustomElement,
             //scProxyConfusion,
             scFetchAbort,
